3c7f1b027cdf7308d74e0a3ce44c53456c7005fb
michaelhebner <mhebner@student.bridgew.edu>
2022-03-17 22:02:53 -0400

    Made a simple layout, embedded a map of Boston, implemented overlay displaying the seperate districts.

---
+++ public/javascripts/map.js
@@ -0,0 +1,13 @@
+
+map = L.map('map').setView([42.3601, -71.0589],13);
+    // load a tile layer
+L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
+{
+  attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+  maxZoom: 17,
+  minZoom: 9
+}).addTo(map);
+
+var districts = new L.GeoJSON.AJAX("json/Police_Districts.geojson")
+districts.addTo(map);
+
