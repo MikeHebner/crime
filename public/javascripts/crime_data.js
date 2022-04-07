a7a49805d10d7f4e1215fc61879d4f116d5b4e46
Kent Loc <kloc@student.bridgew.edu>
2022-04-07 11:40:13 -0400

    test

--- public/javascripts/crime_data.js
+++ public/javascripts/crime_data.js
@@ -29,11 +29,7 @@ function display_recent(data){
         offense.appendChild(node2);
         let offense_code = row.insertCell(3);
         let node3 = document.createTextNode(value['OFFENSE_CODE'])
-<<<<<<< HEAD
-        offense.appendChild(node3);
-=======
         offense_code.appendChild(node3);
->>>>>>> c2ff1380988379df1469601c272df415ddc702a5
         let district = row.insertCell(4);
         let node4 = document.createTextNode(value['DISTRICT'])
         district.appendChild(node4);
@@ -41,11 +37,7 @@ function display_recent(data){
         let node5 = document.createTextNode(value['STREET'])
         street.appendChild(node5);
         let loc = row.insertCell(6);
-<<<<<<< HEAD
-        let node6 = document.createTextNode(value['LOCATION'])
-=======
         let node6 = document.createTextNode(value['Location'])
->>>>>>> c2ff1380988379df1469601c272df415ddc702a5
         loc.appendChild(node6);
     }
     
